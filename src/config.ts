21a022552728f84f434e347676e0544f5a331a99
sdrobisz <sdrobisz@he2b.be>
2022-10-06 23:28:10 +0200

    edit github edit url

--- src/config.ts
+++ src/config.ts
@@ -30,7 +30,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/SebDrobisz/erpg5-django/`;
+export const GITHUB_EDIT_URL = `https://github.com/SebDrobisz/erpg5-django`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
