565dca69aa4f1c39add8f35773080844e3f9f198
sdrobisz <sdrobisz@he2b.be>
2022-10-06 23:09:34 +0200

    change edit url

--- src/config.ts
+++ src/config.ts
@@ -30,7 +30,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://git.esi-bru.be/private/django-site`;
+export const GITHUB_EDIT_URL = `https://github.com/SebDrobisz/erpg5-django/`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
